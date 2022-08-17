9d322df9407f4d502bd42e3bb22f96f202b12261
Trevor Mathiak <tmathiak@gmail.com>
2022-08-17 06:23:30 -0400

    refactored JS, changed some styling and html names

--- script.js
+++ script.js
@@ -1,60 +1,75 @@
-const container = document.getElementById('container');
-const slider = document.getElementById('slider');
-const chooseColor = document.getElementById('choose-color');
-const sliderValue = document.getElementById('slider-value');
-const clear = document.getElementById('clear');
+const container = document.querySelector(".grid-container");
 
-slider.addEventListener('mouseup', gridSize);
-chooseColor.addEventListener('input', draw);
-clear.addEventListener('click', clearGrid);
+const grid = (area) => {
+  let gridArea = area * area;
+  for (i = 0; i <= gridArea; i++) {
+    const divs = document.createElement("div");
+    container.style.gridTemplateColumns = `repeat(${area}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${area}, 1fr)`;
+    container.appendChild(divs).classList.add("grid-item");
+  }
 
-function grid(area) {
-    for (i = 0; i < (area * area); i++) {
-        const divs = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${area}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${area}, 1fr)`;
-        container.appendChild(divs).classList.add('grid-item');
-        divs.addEventListener('mouseover', draw);
-    }
+  handleUI();
 };
 grid(16);
 
-function draw(e) {
-    const divs = container.querySelectorAll('div');
-    document.getElementById('settings').addEventListener('click', event => {
-        if (event.target.id === 'color') {
-            divs.forEach(div => div.addEventListener('mouseover', () =>
-            div.style.backgroundColor = chooseColor.value))
-        } else if (event.target.id === 'rainbow') {
-            divs.forEach(div => div.addEventListener('mouseover', () => {
-                let r = Math.floor(Math.random() * 255);
-                let g = Math.floor(Math.random() * 255);
-                let b = Math.floor(Math.random() * 255);
-                div.style.backgroundColor = `rgb(${r},${g},${b})`
-            }))
-        } else if (event.target.id === 'erase') {
-            divs.forEach(div => div.addEventListener('mouseover', () =>
-            div.style.backgroundColor = '#fff8dc'))
-        } else {
-            divs.forEach(div => div.addEventListener('mouseover', () =>
-            div.style.backgroundColor = '#000000'))
-        }
-    })
-}
-draw();
+const changeGridSize = (() => {
+  const gridSizeSlider = document.querySelector("#slider");
+  const sliderValue = document.querySelector("#slider-value");
 
-function gridSize() {
-    container.innerHTML = '';
-    let gridItems = container.querySelectorAll('div');
-    gridItems.forEach(gridItem => gridItem.remove());
+  gridSizeSlider.addEventListener("mouseup", () => {
+    const divs = container.querySelectorAll("div");
+    container.innerHTML = "";
+    divs.forEach((div) => div.remove());
     grid(slider.value);
-}
+  });
 
-slider.oninput = function() {
+  gridSizeSlider.oninput = function () {
     sliderValue.innerHTML = `${this.value} x ${this.value}`;
-}
+  };
 
-function clearGrid() {
-    const divs = container.querySelectorAll('div');
-    divs.forEach(div => div.style.backgroundColor = '#fff8dc');
-}
+  handleUI();
+})();
+
+function handleUI () {
+  const colorSelector = document.querySelector("#choose-color");
+  const colorButton = document.querySelector(".color-button");
+  const rainbowButton = document.querySelector(".rainbow-button");
+  const eraseButton = document.querySelector(".erase-button");
+  const clearButton = document.querySelector(".clear-button");
+  const divs = container.querySelectorAll("div");
+
+  colorButton.addEventListener("click", () => {
+    divs.forEach((div) =>
+      div.addEventListener(
+        "mouseover",
+        () => (div.style.backgroundColor = colorSelector.value)
+      )
+    );
+  });
+
+  rainbowButton.addEventListener("click", () => {
+    divs.forEach((div) =>
+      div.addEventListener("mouseover", () => {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        div.style.backgroundColor = `rgb(${r},${g},${b})`;
+      })
+    );
+  });
+
+  eraseButton.addEventListener("click", () => {
+    divs.forEach((div) =>
+      div.addEventListener(
+        "mouseover",
+        () => (div.style.backgroundColor = "#fff8dc")
+      )
+    );
+  });
+
+  clearButton.addEventListener("click", () => {
+    const divs = container.querySelectorAll("div");
+    divs.forEach((div) => (div.style.backgroundColor = "#fff8dc"));
+  });
+};
